934edd82ca41712ae4fa00fbec6d31fb828132a8
callmeharis <theharis99@gmail.com>
2024-12-20 01:18:24 +0500

    bug fixes

--- src/components/UpdatedNews.jsx
+++ src/components/UpdatedNews.jsx
@@ -75,7 +75,7 @@ const UpdatedNews = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {loading ? (
           <p>Loading articles...</p>
-        ) : articles.length === 0 ? (
+        ) : articles?.length === 0 ? (
           <p>No articles found.</p>
         ) : (
           articles.map((article) => (
